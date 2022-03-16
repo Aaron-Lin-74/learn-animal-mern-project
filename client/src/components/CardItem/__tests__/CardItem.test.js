55e5fc3e2cb2caf8da6a1dbe1616d932f4eb3166
Aaron Lin <aaronlin74@gmail.com>
2022-03-17 00:01:12 +1100

    Removed the accidentally added semicolon to keep consistency

--- client/src/components/CardItem/__tests__/CardItem.test.js
+++ client/src/components/CardItem/__tests__/CardItem.test.js
@@ -1,7 +1,7 @@
-import { screen, render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import CardItem from '../CardItem';
-import { BrowserRouter } from 'react-router-dom';
+import { screen, render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import CardItem from '../CardItem'
+import { BrowserRouter } from 'react-router-dom'
 
 const mockProps = {
   class: 'animal',
@@ -9,33 +9,33 @@ const mockProps = {
   label: 'Mock Type',
   src: 'http://mock.image.jpg',
   path: '/animal/mockID',
-};
+}
 const MockCardItem = () => {
   return (
     <BrowserRouter>
       <CardItem {...mockProps} />
     </BrowserRouter>
-  );
-};
+  )
+}
 describe('CardItem component', () => {
   it('should render the component', () => {
-    render(<MockCardItem />);
-    screen.getByText('Loading...');
-    screen.getByAltText('loading');
-    screen.getByAltText(mockProps.text);
-    screen.getByRole('heading', { name: 'Animal Name' });
-  });
+    render(<MockCardItem />)
+    screen.getByText('Loading...')
+    screen.getByAltText('loading')
+    screen.getByAltText(mockProps.text)
+    screen.getByRole('heading', { name: 'Animal Name' })
+  })
 
   it('should render the loading image and text before the image is loaded', () => {
-    render(<MockCardItem />);
-    screen.getByText('Loading...');
-    screen.getByAltText('loading');
-  });
+    render(<MockCardItem />)
+    screen.getByText('Loading...')
+    screen.getByAltText('loading')
+  })
 
   it('should disappear the loading image and text after the image is loaded', () => {
-    render(<MockCardItem />);
-    fireEvent.load(screen.getByAltText(mockProps.text));
-    expect(screen.queryByText('Loading...')).toBeNull();
-    expect(screen.queryByAltText('loading')).toBeNull();
-  });
-});
+    render(<MockCardItem />)
+    fireEvent.load(screen.getByAltText(mockProps.text))
+    expect(screen.queryByText('Loading...')).toBeNull()
+    expect(screen.queryByAltText('loading')).toBeNull()
+  })
+})
