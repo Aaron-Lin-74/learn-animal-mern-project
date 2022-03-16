0bad3449d83e4ac692f4405edff955a8784f987c
Aaron Lin <aaronlin74@gmail.com>
2022-03-16 23:41:54 +1100

    Added tests for CardItem component and updated the code

---
+++ client/src/components/CardItem/__tests__/CardItem.test.js
@@ -0,0 +1,41 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CardItem from '../CardItem';
+import { BrowserRouter } from 'react-router-dom';
+
+const mockProps = {
+  class: 'animal',
+  text: 'Animal Name',
+  label: 'Mock Type',
+  src: 'http://mock.image.jpg',
+  path: '/animal/mockID',
+};
+const MockCardItem = () => {
+  return (
+    <BrowserRouter>
+      <CardItem {...mockProps} />
+    </BrowserRouter>
+  );
+};
+describe('CardItem component', () => {
+  it('should render the component', () => {
+    render(<MockCardItem />);
+    screen.getByText('Loading...');
+    screen.getByAltText('loading');
+    screen.getByAltText(mockProps.text);
+    screen.getByRole('heading', { name: 'Animal Name' });
+  });
+
+  it('should render the loading image and text before the image is loaded', () => {
+    render(<MockCardItem />);
+    screen.getByText('Loading...');
+    screen.getByAltText('loading');
+  });
+
+  it('should disappear the loading image and text after the image is loaded', () => {
+    render(<MockCardItem />);
+    fireEvent.load(screen.getByAltText(mockProps.text));
+    expect(screen.queryByText('Loading...')).toBeNull();
+    expect(screen.queryByAltText('loading')).toBeNull();
+  });
+});
