fff93ae89dd32e327ebd44d8b9a0eb2c8ee34bc3
Aaron-Lin-74 <84072071+Aaron-Lin-74@users.noreply.github.com>
2022-03-17 00:13:04 +1100

    Added test suites for the components
    * Added tests for CardItem component and updated the code
    
    * Removed the accidentally  added semicolons to keep the consistency
    
    * Added tests for Footer component

---
+++ client/src/components/Footer/__tests__/Footer.test.js
@@ -0,0 +1,66 @@
+import { screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Footer from '../Footer'
+import { BrowserRouter } from 'react-router-dom'
+
+const MockFooter = () => {
+  return (
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+beforeEach(() => render(<MockFooter />))
+describe('Footer component', () => {
+  it('should render the component', () => {
+    screen.getByRole('link', { name: /contact/i })
+    screen.getByRole('link', { name: /about us/i })
+    screen.getByRole('link', { name: /faq/i })
+    screen.getByRole('link', { name: /terms of use/i })
+    screen.getByRole('link', { name: /learn animals/i })
+    screen.getByText(/learn animals © 2021/i)
+    screen.getByRole('link', { name: /facebook/i })
+    screen.getByRole('link', { name: /instagram/i })
+    screen.getByRole('link', { name: /twitter/i })
+    screen.getByRole('link', { name: /youtube/i })
+  })
+
+  it('should have href attribute for relative buttons', () => {
+    expect(screen.getByRole('link', { name: /contact/i })).toHaveAttribute(
+      'href',
+      '/contact'
+    )
+    expect(screen.getByRole('link', { name: /about us/i })).toHaveAttribute(
+      'href',
+      '/about'
+    )
+    expect(screen.getByRole('link', { name: /faq/i })).toHaveAttribute(
+      'href',
+      '/faq'
+    )
+    expect(screen.getByRole('link', { name: /terms of use/i })).toHaveAttribute(
+      'href',
+      '/terms-of-use'
+    )
+    expect(
+      screen.getByRole('link', { name: /learn animals/i })
+    ).toHaveAttribute('href', '/')
+    expect(screen.getByRole('link', { name: /facebook/i })).toHaveAttribute(
+      'href',
+      'https://www.facebook.com/'
+    )
+    expect(screen.getByRole('link', { name: /instagram/i })).toHaveAttribute(
+      'href',
+      'https://www.instagram.com/'
+    )
+    expect(screen.getByRole('link', { name: /twitter/i })).toHaveAttribute(
+      'href',
+      'https://twitter.com/'
+    )
+    expect(screen.getByRole('link', { name: /youtube/i })).toHaveAttribute(
+      'href',
+      'https://www.youtube.com/'
+    )
+  })
+})
