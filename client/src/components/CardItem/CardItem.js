6702d0084fa23f930ae6404d180da670b63d4fdb
Aaron Lin <aaronlin74@gmail.com>
2022-04-11 19:22:13 +1000

    refactor: update the code based on eslint airbnb config
    - use || to replace unnecessary ternary operator
    - explicitly destructure component props

--- client/src/components/CardItem/CardItem.js
+++ client/src/components/CardItem/CardItem.js
@@ -3,29 +3,29 @@ import { Link } from 'react-router-dom'
 import './CardItem.css'
 import Loading from '../Loading/Loading'
 
-const CardItem = (props) => {
+function CardItem({ className, path, label, src, text }) {
   // Local state to show the loading component when image was loading
   const [loading, setLoading] = React.useState(true)
   const handleOnLoaded = () => {
     setLoading(false)
   }
   return (
-    <div className={`card-item ${props.class ? props.class : ''}`}>
-      <Link className='card-item-link' to={props.path}>
+    <div className={`card-item ${className || ''}`}>
+      <Link className='card-item-link' to={path}>
         {loading && <Loading />}
         <figure
-          className={`card-item-img-wrap ${props.class ? props.class : ''}`}
-          data-category={props.label}
+          className={`card-item-img-wrap ${className || ''}`}
+          data-category={label}
         >
           <img
             className={loading ? 'hidden' : 'card-item-img'}
-            src={props.src}
-            alt={props.text}
+            src={src}
+            alt={text}
             onLoad={handleOnLoaded}
           />
         </figure>
         <div className='card-item-info'>
-          <h3 className='card-item-text'>{props.text}</h3>
+          <h3 className='card-item-text'>{text}</h3>
         </div>
       </Link>
     </div>
