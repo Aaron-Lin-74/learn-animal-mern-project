8606cae38c22b1518ce60e7740830bf5bc3569ce
Aaron Lin <aaronlin74@gmail.com>
2022-05-14 16:05:36 +1000

    refactor: update the server.js

--- server/server.js
+++ server/server.js
@@ -23,13 +23,6 @@ app.use('/api/animals', animals)
 app.use('/api/auth', auth)
 app.use('/api/subscription', subscription)
 
-// Serve static assets
-// app.use(express.static('client/build'))
-
-// // Any request we get except the above route, will load the front-end
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// })
 app.use((req, res, next) => {
   return res.status(404).json({
     error: 'Not Found',
@@ -40,9 +33,6 @@ async function start() {
   try {
     await mongoose.connect(process.env.DB)
     console.log('connected to database')
-    // app.listen(process.env.PORT || 5000, () => {
-    //   console.log('connect to server')
-    // })
   } catch (err) {
     console.log(err.message)
   }
